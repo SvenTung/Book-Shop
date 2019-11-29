5ab255a63e608a3e19ad73e012b3d6ac99aa6887
chad-tung <sven.tung@gmail.com>
2019-11-29 13:51:07 +0000

    Added index, new and show sinatra paths in my author controller file

--- controllers/author_controller.rb
+++ controllers/author_controller.rb
@@ -1,8 +1,23 @@
 require_relative '../models/author'
 
 #index
+get '/authors' do
+  @authors = Author.all
+  erb(:"authors/index")
+end
+
 #show
+get '/authors/:id' do
+  id = params[:id].to_i()
+  @author = Author.find_by_id(id)
+  erb(:"authors/show")
+end
+
 #new
+get '/authors/new' do
+  erb(:"authors/new")
+end
+
 #create
 #edit
 #update
